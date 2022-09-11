b889a5e2ec3206a290df8857784dd800b9d1865b
Sartorius <ratinahirana@gmail.com>
2022-09-12 00:49:34 +0200

    prepareMinify

--- config/db/session.sql
+++ config/db/session.sql
@@ -1,10 +1,10 @@
 symfony local:server:stop
 symfony server:start
 
-11:14:32.186 debug	Hash password: lanatureestverte e716e0cf70d3c6dbd840945d890f074d -- benrand1234
-11:14:32.186 debug	Hash password: lebleuduciel 78e740abec0d61e6dd52452e6d9c2a32 -- harrako1912
-11:14:32.186 debug	Hash password: lelapinblanc 9248153d95104975573f18e2852d6647 -- lalrako3819
-11:14:32.186 debug	Hash password: unetortuegasy 2df68a85f1932ca876926252bbaa0820 -- ionrako6172
+11:14:32.186 debug	Hash password: lanatureestverte e716e0cf70d3c6dbd840945d890f074d -- benrand123
+11:14:32.186 debug	Hash password: lebleuduciel 78e740abec0d61e6dd52452e6d9c2a32 -- harrako912
+11:14:32.186 debug	Hash password: lelapinblanc 9248153d95104975573f18e2852d6647 -- lalrako381
+11:14:32.186 debug	Hash password: unetortuegasy 2df68a85f1932ca876926252bbaa0820 -- ionrako617
 11:14:32.186 debug	Hash password: lelemurientoutnoir c24d593b9266e7b8d0887d0dc7259705 -- mikrama654
 11:14:32.186 debug	Hash password: unnuagedansleciel d064a894fb8645879312b10d366cd604 -- mborako321
 11:14:32.186 debug	Hash password: unesardinedanslamer 9433c9064f0593da5727e2122a193a6e
