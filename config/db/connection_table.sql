a3887db14bf8ef849f40968deb51483fd93ba4ad
Sartorius <ratinahirana@gmail.com>
2022-09-11 19:36:10 +0200

    solve deployment

--- config/db/connection_table.sql
+++ config/db/connection_table.sql
@@ -8,10 +8,14 @@ CREATE TABLE IF NOT EXISTS `ACEA`.`uac_admin` (
   `accounting_write` TINYINT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`));
 
-INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`, `scale_right`) VALUES ((SELECT id FROM mdl_user WHERE username = 'benrand1234'), 'e716e0cf70d3c6dbd840945d890f074d', 'Recteur', NULL, 5);
-INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`, `scale_right`) VALUES ((SELECT id FROM mdl_user WHERE username = 'harrako1912'), '78e740abec0d61e6dd52452e6d9c2a32', 'SG', NULL, 5);
-INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`, `scale_right`) VALUES ((SELECT id FROM mdl_user WHERE username = 'lalrako3819'), '9248153d95104975573f18e2852d6647', 'DAF', NULL, 5);
-INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`) VALUES ((SELECT id FROM mdl_user WHERE username = 'ionrako6172'), '2df68a85f1932ca876926252bbaa0820', 'CGE', NULL);
+-- /!\ IL FAUT AVOIR CRÉE LES UTILISATEURS AVANT DE CHARGER CES REQUÊTES D'ABORD
+
+INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`, `scale_right`) VALUES ((SELECT id FROM mdl_user WHERE username = 'benrand123'), 'e716e0cf70d3c6dbd840945d890f074d', 'Recteur', NULL, 5);
+INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`, `scale_right`) VALUES ((SELECT id FROM mdl_user WHERE username = 'harrako912'), '78e740abec0d61e6dd52452e6d9c2a32', 'SG', NULL, 5);
+INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`, `scale_right`) VALUES ((SELECT id FROM mdl_user WHERE username = 'lalrako381'), '9248153d95104975573f18e2852d6647', 'DAF', NULL, 5);
+INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`) VALUES ((SELECT id FROM mdl_user WHERE username = 'ionrako617'), '2df68a85f1932ca876926252bbaa0820', 'CGE', NULL);
+INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`) VALUES ((SELECT id FROM mdl_user WHERE username = 'mikrama654'), 'c24d593b9266e7b8d0887d0dc7259705', 'Agent', NULL);
+INSERT INTO `ACEA`.`uac_admin` (`id`, `pwd`, `role`, `last_connection`) VALUES ((SELECT id FROM mdl_user WHERE username = 'mborako321'), 'd064a894fb8645879312b10d366cd604', 'Agent', NULL);
 
 -- SHOW USERS
 
@@ -22,3 +26,36 @@ CREATE TABLE IF NOT EXISTS `ACEA`.`uac_showuser` (
   `secret` INT UNSIGNED NULL,
   `last_update` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`username`));
+
+
+
+  -- Insert
+    INSERT IGNORE INTO uac_showuser (roleid, username)
+    SELECT MAX(roleid), username FROM mdl_role_assignments mra
+                                  JOIN mdl_role mr ON mr.id = mra.roleid
+                                  JOIN mdl_user mu ON mu.id = mra.userid
+    GROUP BY username;
+  -- Add secret
+    UPDATE uac_showuser SET secret = 3000000000 + FLOOR(RAND()*1000000000), last_update = NOW() WHERE secret IS NULL;
+  -- Control to save !
+  SELECT
+      mu.id AS ID,
+            mu.username AS USERNAME,
+             uas.secret AS SECRET,
+             CONCAT(CAST(uas.secret AS CHAR), UPPER(uas.username)) AS PAGE,
+             mr.shortname AS ROLE_SHORTNAME,
+             UPPER(mu.firstname) AS FIRSTNAME,
+             UPPER(mu.lastname) AS LASTNAME,
+             mu.email AS EMAIL,
+             mu.phone1 AS PHONE,
+             mu.address AS ADDRESS,
+             mu.city AS CITY,
+             mu.phone2 AS PARENT_PHONE,
+             mf.contextid AS PIC_CONTEXT_ID,
+             mu.picture AS PICTURE_ID,
+             SUBSTRING(mf.contenthash, 1, 2) AS D1,
+             SUBSTRING(mf.contenthash, 3, 2) AS D2,
+             mf.contenthash AS FILENAME
+    FROM mdl_user mu JOIN uac_showuser uas ON mu.username = uas.username
+             JOIN mdl_role mr ON mr.id = uas.roleid
+             LEFT JOIN mdl_files mf ON mu.picture = mf.id;
--- config/db/session.sql
+++ config/db/session.sql
@@ -5,8 +5,8 @@ symfony server:start
 11:14:32.186 debug	Hash password: lebleuduciel 78e740abec0d61e6dd52452e6d9c2a32 -- harrako1912
 11:14:32.186 debug	Hash password: lelapinblanc 9248153d95104975573f18e2852d6647 -- lalrako3819
 11:14:32.186 debug	Hash password: unetortuegasy 2df68a85f1932ca876926252bbaa0820 -- ionrako6172
-11:14:32.186 debug	Hash password: lelemurientoutnoir c24d593b9266e7b8d0887d0dc7259705
-11:14:32.186 debug	Hash password: unnuagedansleciel d064a894fb8645879312b10d366cd604
+11:14:32.186 debug	Hash password: lelemurientoutnoir c24d593b9266e7b8d0887d0dc7259705 -- mikrama654
+11:14:32.186 debug	Hash password: unnuagedansleciel d064a894fb8645879312b10d366cd604 -- mborako321
 11:14:32.186 debug	Hash password: unesardinedanslamer 9433c9064f0593da5727e2122a193a6e
 11:14:32.186 debug	Hash password: lelionestleroi 3182a20c71d20d919cb1f3b3035d5924
 11:14:32.186 debug	Hash password: unrequindanslocean 3165cef3bc67568c7d30f5a184f43766
