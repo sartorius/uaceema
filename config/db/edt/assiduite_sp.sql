676f93e4d47074384c52ecfc55494f65a0716fdb
Sartorius <ratinahirana@gmail.com>
2022-09-25 19:05:09 +0200

    compute late operation missing cohort

--- config/db/edt/assiduite_sp.sql
+++ config/db/edt/assiduite_sp.sql
@@ -10,10 +10,15 @@ BEGIN
     DECLARE inv_flow_code	CHAR(7);
     DECLARE concurrent_flow INT;
     DECLARE inv_flow_id	BIGINT;
+    DECLARE late_definition	VARCHAR(50);
+    DECLARE wait_b_compute	VARCHAR(50);
 
     DECLARE inv_edt_id	BIGINT;
+    DECLARE inv_edt_starts	TINYINT;
     DECLARE count_courses_todo INT;
-    DECLARE iterator_courses_todo INT;
+
+    DECLARE i INT default 0;
+    SET i = 0;
 
 
 
@@ -29,6 +34,9 @@ BEGIN
 
         INSERT INTO uac_working_flow (flow_code, status, working_date, working_part, last_update) VALUES (inv_flow_code, 'NEW', CURRENT_DATE, 0, NOW());
         SELECT LAST_INSERT_ID() INTO inv_flow_id;
+
+        SELECT par_value INTO late_definition FROM uac_param WHERE key_code = 'ASSLATE';
+        SELECT par_value INTO wait_b_compute FROM uac_param WHERE key_code = 'ASSWAIT';
 
 
         -- If the parameter is null, we consider the day of today !
@@ -58,12 +66,104 @@ BEGIN
                 CONCAT(
                   CASE WHEN (CHAR_LENGTH(ue.hour_starts_at + ue.duration_hour) = 1)
                     THEN CONCAT('0', ue.hour_starts_at + ue.duration_hour) ELSE ue.hour_starts_at + ue.duration_hour END,
-                    ':20:00'), TIME) < inv_time;
+                    -- wait_b_compute ':20:00'
+                    wait_b_compute), TIME) < inv_time;
 
+        -- ********************************************
+        -- ********************************************
+        -- ****************** START *******************
+        -- ********************************************
+        -- ********************************************
+        WHILE i < count_courses_todo DO
+
+          -- INITIALIZATION
+          SELECT MAX(id) INTO inv_edt_id
+              FROM uac_edt ue WHERE ue.compute_late_status = 'NEW'
+              AND ue.day = inv_date
+              -- We take only not empty courses
+              AND ue.duration_hour > 0
+              AND CONVERT(
+                    CONCAT(
+                      CASE WHEN (CHAR_LENGTH(ue.hour_starts_at + ue.duration_hour) = 1)
+                        THEN CONCAT('0', ue.hour_starts_at + ue.duration_hour) ELSE ue.hour_starts_at + ue.duration_hour END,
+                        -- wait_b_compute ':20:00'
+                        wait_b_compute), TIME) < inv_time;
+
+          -- Initialize the hour start at
+          SELECT hour_starts_at INTO inv_edt_starts FROM uac_edt WHERE id = inv_edt_id;
+
+
+
+          -- ABS *** *** *** *** *** *** *** ***
+          -- MISSING LINK with the correct COHORT ID !
+          -- Final list of student missing or late over 10min !
+          INSERT IGNORE INTO uac_assiduite (flow_id, user_id, edt_id, scan_id, status)
+          SELECT inv_flow_id, mu.id, inv_edt_id, NULL, 'ABS' FROM mdl_user mu
+          		 JOIN uac_showuser uas ON mu.username = uas.username
+          WHERE mu.username NOT IN (
+          		SELECT t_student_in.username
+          		FROM(
+          					-- List of people who entered but not exit before 7:00
+          					select mu.username, in_out, max(usa.scan_time) AS scan_in from uac_scan usa
+          					join mdl_user mu on usa.user_id = mu.id
+                    -- late_definition is like ':10:00' Saying max 10 min late
+          					WHERE usa.scan_time < CONVERT(CONCAT(CASE WHEN (CHAR_LENGTH(inv_edt_starts) = 1) THEN CONCAT('0', inv_edt_starts) ELSE inv_edt_starts END, late_definition), TIME)
+                    AND usa.scan_date = inv_date
+                    group by mu.username, in_out
+          					having in_out = 'I'
+          					) t_student_in
+          		);
+
+          -- LAT *** *** *** *** *** *** *** ***
+          -- MISSING LINK with the correct COHORT ID !
+          -- Final list of student late 10min max !
+          INSERT IGNORE INTO uac_assiduite (flow_id, user_id, edt_id, scan_id, status)
+          SELECT inv_flow_id, mu.id, inv_edt_id, max_scan.id AS scan_id, 'LAT' FROM (
+          -- List of people who entered between 7:00 and 7:10
+          SELECT mu.username, in_out, max(usa.scan_time) AS scan_in from uac_scan usa
+          JOIN mdl_user mu on usa.user_id = mu.id
+          WHERE usa.scan_time > CONVERT(CONCAT(CASE WHEN (CHAR_LENGTH(inv_edt_starts) = 1) THEN CONCAT('0', inv_edt_starts) ELSE inv_edt_starts END, ':00:00'), TIME)
+          AND usa.scan_time < CONVERT(CONCAT(CASE WHEN (CHAR_LENGTH(inv_edt_starts) = 1) THEN CONCAT('0', inv_edt_starts) ELSE inv_edt_starts END, late_definition), TIME)
+          AND usa.scan_date = inv_date
+          GROUP BY mu.username, in_out
+          HAVING in_out = 'I'
+          ) t_student_in JOIN uac_showuser uas ON t_student_in.username = uas.username
+          			   JOIN mdl_user mu on mu.username = uas.username
+          			   	  JOIN uac_scan max_scan ON max_scan.user_id = mu.id
+          			   	  							  AND max_scan.scan_time = scan_in
+          			   	  							  AND max_scan.in_out = 'I';
+
+          -- PON *** *** *** *** *** *** *** ***
+          -- MISSING LINK with the correct COHORT ID !
+          -- Student on time
+          INSERT IGNORE INTO uac_assiduite (flow_id, user_id, edt_id, scan_id, status)
+          SELECT inv_flow_id, mu.id, inv_edt_id, max_scan.id AS scan_id, 'PON' FROM (
+          -- List of people who entered but not exit before 7:00
+          SELECT mu.username, in_out, max(usa.scan_time) AS scan_in from uac_scan usa
+          JOIN mdl_user mu on usa.user_id = mu.id
+          WHERE usa.scan_time < CONVERT(CONCAT(CASE WHEN (CHAR_LENGTH(inv_edt_starts) = 1) THEN CONCAT('0', inv_edt_starts) ELSE inv_edt_starts END, ':00:00'), TIME)
+          AND usa.scan_date = inv_date
+          GROUP BY mu.username, in_out
+          HAVING in_out = 'I'
+          ) t_student_in JOIN uac_showuser uas ON t_student_in.username = uas.username
+          			   JOIN mdl_user mu on mu.username = uas.username
+          			   	  JOIN uac_scan max_scan ON max_scan.user_id = mu.id
+          			   	  							  AND max_scan.scan_time = scan_in
+          			   	  							  AND max_scan.in_out = 'I';
+
+          UPDATE uac_edt SET compute_late_status = 'END', last_update = NOW() WHERE id = inv_edt_id;
+          SET i = i + 1;
+        END WHILE;
+
+        -- ********************************************
+        -- ********************************************
+        -- ****************** SEND ********************
+        -- ********************************************
+        -- ********************************************
 
         -- BODY END
         -- End of the flow correctly
-        UPDATE uac_working_flow SET status = 'END', comment = CONCAT('EDT updated: ', count_courses_todo) WHERE id = inv_flow_id;
+        UPDATE uac_working_flow SET status = 'END', last_update = NOW(), comment = CONCAT('EDT updated: ', count_courses_todo) WHERE id = inv_flow_id;
 
     END IF;
 END$$
