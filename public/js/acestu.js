d7bb7691005179c0ea96c47b14aa6e22ab8741e7
Sartorius <ratinahirana@gmail.com>
2022-11-16 23:05:57 +0100

    upd check scan

--- public/js/acestu.js
+++ public/js/acestu.js
@@ -141,6 +141,172 @@ function goToSTU(page){
   $("#mg-stu-page-form").submit();
 }
 
+/***********************************************************************************************************/
+
+function loadLastScanGrid(){
+
+      if(window.screen.availWidth < 1100){
+          lastScanFields = [
+              { name: "USERNAME",
+                title: 'Scan',
+                type: "text",
+                align: "left",
+                width: 50,
+                css: "cell-recap-l-num",
+                headercss: "cell-recap-hd"
+              },
+              //Default width is auto
+              { name: "SCAN_TIME",
+                title: 'Heure',
+                type: "text",
+                width: 30,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l"
+              },
+              //Default width is auto
+              { name: "IN_OUT",
+                title: 'E/S',
+                type: "text",
+                width: 20,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l",
+                itemTemplate: function(value, item) {
+                  let val = '';
+                  if(value == 'I'){
+                    val = 'E';
+                  }
+                  else{
+                    val = 'S';
+                  }
+                  return val;
+                }
+              },
+              //Default width is auto
+              { name: "SCAN_STATUS",
+                title: 'Status',
+                type: "text",
+                width: 33,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l",
+                itemTemplate: function(value, item) {
+                  let val = '';
+                  if(value == 'END'){
+                    val = 'Validé';
+                  }
+                  else if(value == 'NEW'){
+                    val = '<i class="recap-lat">En cours</i>';
+                  }
+                  else {
+                    val = '<i class="recap-mis">Erreur</i>';
+                  }
+                  return val;
+                }
+              }
+          ];
+
+      }else{
+
+          lastScanFields = [
+              { name: "USERNAME",
+                title: 'Scan',
+                type: "text",
+                align: "left",
+                width: 40,
+                css: "cell-recap-l-num",
+                headercss: "cell-recap-hd"
+              },
+              //Default width is auto
+              { name: "FIRSTNAME",
+                title: "Prénom",
+                type: "text",
+                width: 60,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l"
+              },
+              //Default width is auto
+              { name: "LASTNAME",
+                title: "Nom de famille",
+                type: "text",
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l"
+              },
+              //Default width is auto
+              { name: "SCAN_DATE",
+                title: "Date",
+                type: "text",
+                width: 20,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l"
+              },
+              //Default width is auto
+              { name: "SCAN_TIME",
+                title: 'Heure',
+                type: "text",
+                width: 30,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l"
+              },
+              //Default width is auto
+              { name: "IN_OUT",
+                title: 'E/S',
+                type: "text",
+                width: 30,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l",
+                itemTemplate: function(value, item) {
+                  let val = '';
+                  if(value == 'I'){
+                    val = '<i class="ass-in-time"><span class="icon-arrow-circle-right nav-icon-fa-sm nav-text"></span>&nbsp;Entrée</i>';
+                  }
+                  else{
+                    val = '<i class="ass-out-time"><span class="icon-arrow-circle-left nav-icon-fa-sm nav-text"></span>&nbsp;Sortie</i>';
+                  }
+                  return val;
+                }
+              },
+              //Default width is auto
+              { name: "SCAN_STATUS",
+                title: 'Status',
+                type: "text",
+                width: 33,
+                headercss: "cell-recap-hd",
+                css: "cell-recap-l",
+                itemTemplate: function(value, item) {
+                  let val = '';
+                  if(value == 'END'){
+                    val = 'Validé';
+                  }
+                  else if(value == 'NEW'){
+                    val = '<i class="recap-lat">En cours</i>';
+                  }
+                  else {
+                    val = '<i class="recap-mis">Erreur</i>';
+                  }
+                  return val;
+                }
+              }
+          ];
+
+      };
+
+    $("#jsGridLastScanSTU").jsGrid({
+        height: "auto",
+        width: "100%",
+        noDataContent: "Aucun scan n'a été enregistré aujourd'hui",
+        pageIndex: 1,
+        pageSize: 50,
+        pagerFormat: "Pages: {first} {prev} {pages} {next} {last}    {pageIndex} sur {pageCount}",
+        pagePrevText: "Prec",
+        pageNextText: "Suiv",
+        pageFirstText: "Prem",
+        pageLastText: "Dern",
+
+        sorting: true,
+        paging: true,
+        data: dataLastScanToJsonArray,
+        fields: lastScanFields
+    });
+}
 
 /***********************************************************************************************************/
 
@@ -152,6 +318,11 @@ $(document).ready(function() {
     // Do nothing
     initAllSTUGrid();
     loadAllSTUGrid();
+  }
+  if($('#mg-graph-identifier').text() == 'chk-sca'){
+    // Do something on chk-sca
+    // Do nothing
+    loadLastScanGrid();
   }
   else if($('#mg-graph-identifier').text() == 'xxx'){
     // Do something
--- src/Controller/AdminSTUController.php
+++ src/Controller/AdminSTUController.php
@@ -63,4 +63,47 @@ class AdminSTUController extends AbstractController
     return new Response($content);
   }
 
+
+  public function checklastscan(Environment $twig, LoggerInterface $logger)
+  {
+
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    //'scale_right' => ConnectionManager::whatScaleRight()
+
+    $scale_right = ConnectionManager::whatScaleRight();
+
+
+
+    if(isset($scale_right) && ($scale_right > -1)){
+        $logger->debug("Firstname: " . $_SESSION["firstname"]);
+
+        $allstu_query = " SELECT UPPER(uls.scan_username) AS USERNAME, DATE_FORMAT(uls.scan_date, '%d/%m') AS SCAN_DATE, uls.scan_time AS SCAN_TIME, uls.status AS SCAN_STATUS, uls.in_out AS IN_OUT, "
+                        . " CASE WHEN uls.status = 'END' THEN REPLACE(UPPER(mu.firstname), \"'\", \" \") ELSE 'na' END AS FIRSTNAME, "
+                        . " CASE WHEN uls.status = 'END' THEN REPLACE(UPPER(mu.lastname), \"'\", \" \") ELSE 'na' END AS LASTNAME "
+                        . " FROM uac_load_scan uls LEFT JOIN mdl_user mu ON UPPER(mu.username) = UPPER(uls.scan_username) "
+                        . " WHERE uls.scan_date = CURRENT_DATE  ORDER BY uls.id desc;";
+
+
+        $dbconnectioninst = DBConnectionManager::getInstance();
+        $result_all_stu = $dbconnectioninst->query($allstu_query)->fetchAll(PDO::FETCH_ASSOC);
+        $logger->debug("Show me: " . count($result_all_stu));
+
+        $content = $twig->render('Admin/STU/checklastscan.html.twig', ['amiconnected' => ConnectionManager::amIConnectedOrNot(),
+                                                                  'firstname' => $_SESSION["firstname"],
+                                                                  'lastname' => $_SESSION["lastname"],
+                                                                  'id' => $_SESSION["id"],
+                                                                  'result_all_stu'=>$result_all_stu,
+                                                                  'scale_right' => ConnectionManager::whatScaleRight(),
+                                                                  'errtype' => '']);
+
+    }
+    else{
+        // Error Code 404
+        $content = $twig->render('Static/error736.html.twig', ['amiconnected' => ConnectionManager::amIConnectedOrNot(), 'scale_right' => ConnectionManager::whatScaleRight()]);
+    }
+    return new Response($content);
+  }
+
 }
