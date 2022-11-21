669ff3634421a28f6ffea5984a46754f19ecc855
Sartorius <ratinahirana@gmail.com>
2022-11-21 21:44:34 +0100

    upd scan nbr

--- config/db/report_graph/report.sql
+++ config/db/report_graph/report.sql
@@ -1,6 +1,6 @@
 DROP VIEW IF EXISTS rep_course_dash;
 CREATE VIEW rep_course_dash AS
-SELECT CONCAT(vcc.niveau, '/', vcc.mention, '/', vcc.parcours, '/', vcc.groupe) AS CLASSE,
+SELECT vcc.short_classe AS CLASSE,
 	CASE
                       WHEN uel.day_code = 1 THEN "Lundi"
                       WHEN uel.day_code = 2 THEN "Mardi"
@@ -29,13 +29,13 @@ SELECT CONCAT(vcc.niveau, '/', vcc.mention, '/', vcc.parcours, '/', vcc.groupe) 
     CONCAT(uel.hour_starts_at, 'h00') AS DEBUT_COURS,
     CASE WHEN t_abs.abs_cnt IS NULL THEN 0 ELSE t_abs.abs_cnt END AS NBR_ABS,
     CASE WHEN t_qui.qui_cnt IS NULL THEN 0 ELSE t_qui.qui_cnt END AS NBR_QUI, t_cohort_count.cohort_count AS NUMBER_STUD,
-    CASE WHEN uao.id IS NULL THEN 'NON' ELSE 'OUI' END AS OFF_DAY
+    CASE WHEN uao.id IS NULL THEN 'NON' ELSE 'OUI' END AS OFF_DAY, uel.day AS TECH_DAY, uel.hour_starts_at AS TECH_HOUR
 	FROM (
 		SELECT edt_id AS abs_edt_id, count(1) AS abs_cnt
 		FROM uac_assiduite
 		WHERE status = 'ABS'
 		GROUP BY edt_id
-	 ) t_abs RIGHT JOIN uac_edt_line uel ON uel.id = t_abs.abs_edt_id
+  ) t_abs RIGHT JOIN uac_edt_line uel ON uel.id = t_abs.abs_edt_id
 	 LEFT JOIN (
 		SELECT edt_id AS qui_edt_id, count(1) AS qui_cnt
 		FROM uac_assiduite
@@ -52,7 +52,8 @@ SELECT CONCAT(vcc.niveau, '/', vcc.mention, '/', vcc.parcours, '/', vcc.groupe) 
 	) t_cohort_count ON t_cohort_count.vshcohort_id = uem.cohort_id
 	LEFT JOIN uac_assiduite_off uao ON uao.working_date = uel.day
 	WHERE uel.duration_hour > 0
-	ORDER BY uel.day, uel.hour_starts_at DESC;
+	AND uel.day <= CURRENT_DATE
+	ORDER BY uel.day DESC;
 
 
 DROP VIEW IF EXISTS rep_no_exit;
